408a2d31fd077cecf975f0c38d7f545593e50337
monal94 <monal94@gmail.com>
2017-01-18 21:00:35 +0530

    Complete svm.py

--- svm.py
+++ svm.py
@@ -1,11 +1,90 @@
+import matplotlib.pyplot as plt
 from sklearn import datasets
+from sklearn import metrics
 from sklearn import svm
 from sklearn.cross_validation import train_test_split
+from sklearn.grid_search import GridSearchCV
+from sklearn.manifold import Isomap
 
 digits = datasets.load_digits()
 X_train, X_test, y_train, y_test, images_train, images_test = train_test_split(digits.data, digits.target,
                                                                                digits.images, test_size=0.25,
                                                                                random_state=42)
 
-svc_model = svm.SVC(gamma=0.001, C=100, kernel='linear')
+svc_model = svm.SVC(gamma=0.001, C=100, kernel='rbf')
 svc_model.fit(X_train, y_train)
+print(svc_model.score(X_test, y_test))
+
+# Set the parameter candidates
+parameter_candidates = [
+    {'C': [1, 10, 100, 1000], 'kernel': ['linear']},
+    {'C': [1, 10, 100, 1000], 'gamma': [0.001, 0.0001], 'kernel': ['rbf']},
+]
+
+# Create a classifier with the parameter candidates
+clf = GridSearchCV(estimator=svm.SVC(), param_grid=parameter_candidates, n_jobs=-1)
+
+# Train the classifier on training data
+clf.fit(X_train, y_train)
+
+# Print out the results
+print('Best score for training data:', clf.best_score_)
+print('Best `C`:', clf.best_estimator_.C)
+print('Best kernel:', clf.best_estimator_.kernel)
+print('Best `gamma`:', clf.best_estimator_.gamma)
+
+# Apply the classifier to the test data, and view the accuracy score
+clf.score(X_test, y_test)
+
+# Train and score a new classifier with the grid search parameters
+print(svm.SVC(C=10, kernel='rbf', gamma=0.001).fit(X_train, y_train).score(X_test, y_test))
+
+# Assign the predicted values to `predicted`
+predicted = svc_model.predict(X_test)
+
+# Zip together the `images_test` and `predicted` values in `images_and_predictions`
+images_and_predictions = list(zip(images_test, predicted))
+
+# For the first 4 elements in `images_and_predictions`
+for index, (image, prediction) in enumerate(images_and_predictions[:4]):
+    # Initialize subplots in a grid of 1 by 4 at positions i+1
+    plt.subplot(1, 4, index + 1)
+    # Don't show axes
+    plt.axis('off')
+    # Display images in all subplots in the grid
+    plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
+    # Add a title to the plot
+    plt.title('Predicted: ' + str(prediction))
+
+# Show the plot
+plt.show()
+
+# Print the classification report of `y_test` and `predicted`
+print(metrics.classification_report(y_test, predicted))
+
+# Print the confusion matrix of `y_test` and `predicted`
+print(metrics.confusion_matrix(y_test, predicted))
+
+# Create an isomap and fit the `digits` data to it
+X_iso = Isomap(n_neighbors=10).fit_transform(X_train)
+
+# Compute cluster centers and predict cluster index for each sample
+predicted = svc_model.predict(X_train)
+
+# Create a plot with subplots in a grid of 1X2
+fig, ax = plt.subplots(1, 2, figsize=(8, 4))
+
+# Adjust the layout
+fig.subplots_adjust(top=0.85)
+
+# Add scatterplots to the subplots
+ax[0].scatter(X_iso[:, 0], X_iso[:, 1], c=predicted)
+ax[0].set_title('Predicted labels')
+ax[1].scatter(X_iso[:, 0], X_iso[:, 1], c=y_train)
+ax[1].set_title('Actual Labels')
+
+# Add title
+fig.suptitle('Predicted versus actual labels', fontsize=14, fontweight='bold')
+
+# Show the plot
+plt.show()
